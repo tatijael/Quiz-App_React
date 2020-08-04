2d3deabafe474c14a142541559b613c55cf1761f
Tatiana Saucedo <tatijael@gmail.com>
2020-08-04 11:18:46 -0300

    :lipstick: Se agrega estilos a las Options

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -1,25 +1,56 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function Options({data, validate, selected}) {
+const colors = {
+    blanco: { color: "#000000" },
+    correcta: { color: "#78C2AC" },
+    incorrecta:{color:"#FF7851"}
+}
+  
+function Options({data}) {
+    const [selected, setSelected] = useState(null);
+    
+    
+    const validate = (key) => {
+        console.log("soy el target", key)
+        setSelected(key)
+    }
+
+    // option casillero en si
+    // data.answer respuesta si es correcto
+    // selected opcion elegida por el usuario
+
+    // B es la elegida por el usuario y la C es la correcta
+    // no fue selecionada y no es la respuesta correcta => blanco
+    // fue seleccionada y no coincide con answer => incorrecto 
+    // no fue selecionada y coincide con answer => correcta
+ 
+    // C es la elegida por el usuario y la C es la correcta
+    // no fue selecionada y no coincide con answer => blanco
+    // fue selecionada y coincide con answer => correcta
+  
+    
+    const getStatus = (option)=>{
+        if(!selected) return colors.blanco;
+
+        if (option == data.answer) return colors.correcta;
+
+        return (option == selected)? colors.incorrecta : colors.blanco;
+
+    }
 
     return (
         <div>
         {Object.keys(data.options).map(key => {
             return(
-                <div className="form-check" key={key}>
+                <label className="form-check" key={key} style={getStatus(key)}>
                     <input
                         className="form-check-input"
                         type="radio"
-                        name={key}
-                        id={key}
-                        value="option1"
-                        onChange={validate}
-                        checked={key === selected} 
+                        name="question"
+                        onChange={()=>validate(key)}
                     />
-                    <label className="form-check-label" htmlFor={key}>
                         {data.options[key]}
-                    </label>
-                </div>
+                </label>
             )
         }
         )}
--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -3,18 +3,11 @@ import Options from './Options';
 
 
 const Question = ({data}) => {
-    const [selected, setSelected] = useState([]);
-    
-    
-    const validate = (event) => {
-        console.log("soy el target", event.target)
-        setSelected(event.target.name)
-    }
 
     return (
         <div>
             <h4>{data.question}</h4>
-            <Options data={data} validate={validate} selected={selected}/>
+            <Options data={data} />
         </div>
     )
 }
